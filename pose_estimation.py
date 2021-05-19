614836006993daf232b3c9e4bc4956e4030dff4f
2014031930 <inseo8070@naver.com>
2021-05-19 22:26:48 +0900

    get input images's pose landmarks

--- pose_estimation.py
+++ pose_estimation.py
@@ -10,13 +10,42 @@ image_path = './images/'
 print(os.listdir(image_path))
 images = {name: cv2.imread(image_path + name) for name in os.listdir(image_path)}
 
-# for name, image in images.items():
-#     print(name)
-#     cv2.imshow(name, image)
-
-# key = cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-
+for name, image in images.items():
+    print(name)
+    cv2.imshow(name, image)
 
 mp_pose = mp.solutions.pose
+
+help(mp_pose.Pose)
+# Prepare DrawingSpec for drawing the face landmarks later.
+mp_drawing = mp.solutions.drawing_utils 
+drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+
+with mp_pose.Pose(
+    static_image_mode=True, min_detection_confidence=0.5, model_complexity=2) as pose:
+  for name, image in images.items():
+    # Convert the BGR image to RGB and process it with MediaPipe Pose.
+    results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    # Print nose landmark.
+    image_hight, image_width, _ = image.shape
+    if not results.pose_landmarks:
+      continue
+    print(
+      f'Nose coordinates: ('
+      f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].x * image_width}, '
+      f'{results.pose_landmarks.landmark[mp_pose.PoseLandmark.NOSE].y * image_hight})'
+    )
+
+    # Draw pose landmarks.
+    print(f'Pose landmarks of {name}:')
+    annotated_image = image.copy()
+    mp_drawing.draw_landmarks(
+        image=annotated_image,
+        landmark_list=results.pose_landmarks,
+        connections=mp_pose.POSE_CONNECTIONS,
+        landmark_drawing_spec=drawing_spec,
+        connection_drawing_spec=drawing_spec)
+    cv2.imshow(name, annotated_image)
+
+key = cv2.waitKey(0)
+cv2.destroyAllWindows()
